a61842db3c85394427aae55d160135c1b83dc192
rwilleyii <rwilley@me.com>
2017-11-12 20:10:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,13 @@ function BoardMember (name, homeState, training) {
   this.veto = function() {
     return("No, I must disagree");
   }
+  this.approve = function() {
+    return("You can do that!")
+  }
+  this.doCharity = function() {
+    return("I like to help people.")
+  }
+  this.releasePressStatement = function() {
+    
+  }
 }
