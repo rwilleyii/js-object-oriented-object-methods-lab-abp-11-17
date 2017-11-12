b808139cb438cda837d072683139de0ae8dc8ec3
rwilleyii <rwilley@me.com>
2017-11-12 20:07:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-function BoardMember ( name, homeState, training) {
+function BoardMember (name, homeState, training) {
   this.name = name
   this.homeState = homeState
   this.training = training
   this.veto = function() {
-    console.log("No, I must disagree");
+    return("No, I must disagree");
   }
 }
